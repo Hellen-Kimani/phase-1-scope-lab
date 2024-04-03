26fed5e5d49e121886da640b12e21a79bcb8b12f
hellen kimani <astridlee80@gmail.com>
2024-04-04 00:20:37 +1000

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // Attempt to change the constant
+}
+
+const leastFavoriteCustomer = 'someone'; // Declaring a constant in global scope
